923e74d1410748f0633b6359866127ba672b4c91
fclivaz42 <fclivaz@pm.me>
2024-07-11 22:32:26 +0200

    i cant be bothered

--- srcs/requirements/wordpress/tools/config.sh
+++ srcs/requirements/wordpress/tools/config.sh
@@ -1,6 +1,37 @@
+cd /etc/php83/php-fpm.d/
+sed -i "s/127.0.0.1://g" www.conf
+sed -i "s/;clear/clear/g" www.conf
+
 cd /var/www/wordpress
-sed -i "s/'database_name_here'/'$SQL_DATABASE'/g" wp-config-sample.php
-sed -i "s/'username_here'/'$SQL_DATABASE'/g" wp-config-sample.php
-sed -i "s/'password_here'/'$SQL_PASSWORD'/g" wp-config-sample.php
-sed -i "s/'localgist'/'mariadb:3306'/g" wp-config-sample.php
+sed -i "s/'database_name_here'/'$SQL_DB'/g" wp-config-sample.php
+sed -i "s/'username_here'/'$SQL_DB'/g" wp-config-sample.php
+sed -i "s/'password_here'/'$SQL_PW'/g" wp-config-sample.php
+sed -i "s/'localhost'/'mariadb:3306'/g" wp-config-sample.php
 mv wp-config-sample.php wp-config.php
+
+sleep 10
+
+wp-cli core install --url=$DOM_NAME/ \
+					--title=$SITE_TITLE\
+					--admin_user=$WP_ADMINNAME\
+					--admin_password=$WP_ADMINPASS\
+					--admin_email=$WP_ADMINMAIN\
+					--skip-email\
+					--allow-root
+wp-cli user create	$WP_USER\
+					$WP_MAIL\
+					--user_pass=$WP_USERPASS\
+					--role=author\
+					--allow-root
+wp-cli theme install astra\
+					--activate\
+					--allow-root
+wp-cli plugin install redis-cache\
+					--activate\
+					--allow-root
+wp-cli plugin update\
+					--all\
+					--allow-root
+wp-cli redis enable --allow-root
+
+php-fpm83 -F
